d2b4792bea0a9763449ff4b6d262d88090727d0c
krjoe90code <krjoe90code@gmail.com>
2020-12-04 22:33:00 +0000

    BubbleSort-Recursive-and-Iterative-method

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+let arr = [5, 1, 7, 4, 3, 6, 2, 8, 500, 99, 300];
+function bubbleSort(arr) {
+  for (i = 0; i < arr.length; i++) {
+    let a1 = arr[i];
+    let a2 = arr[i + 1];
+
+    if (arr[i] > arr[i + 1]) {
+      arr[i] = a2;
+      arr[i + 1] = a1;
+      // return bubbleSort(arr);  -- use this for recursive method
+      i = 0; // Use this for iterative method
+    }
+  }
+}
+
+bubbleSort(arr);
+console.log(arr);
